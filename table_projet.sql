3af371e6ed7841ea48764d224dfbeb9a8fab2524
riad73 <58702474+riad73@users.noreply.github.com>
2021-04-13 16:37:13 +0200

    Update table_projet.sql

--- table_projet.sql
+++ table_projet.sql
@@ -14,35 +14,40 @@ CREATE TABLE Clients(
 	CONSTRAINT cl_pk PRIMARY KEY (numero_client)
 );
 	
+	
+CREATE TABLE Paniers(
+	numero_commande INTEGER NOT NULL,
+	numero_client INTEGER NOT NULL
+);
+
 CREATE TABLE Commandes(
 	numero_commande INTEGER NOT NULL,
 	numero_client INTEGER NOT NULL,
-	reference_article INTEGER NOT NULL,
 	adresse_livraison TEXT NOT NULL,
 	date_achat DATE NOT NULL,
 	statut TEXT NOT NULL,
 	CONSTRAINT com_pk PRIMARY KEY (numero_commande),
-	CONSTRAINT com_ck CHECK (statut in ("expediee", "transit", "livrée"))
+	CONSTRAINT com_ck CHECK (statut in ("expediée", "transit", "livrée"))
 );
 	
 CREATE TABLE Articles(
 	reference_article INTEGER NOT NULL,
-	nom_article TEXT NOT NULL,
 	CONSTRAINT art_pk PRIMARY KEY (reference_article)
 );
 
-CREATE TABLE Type_articles(
+CREATE TABLE TypeArticles(
+	nom_article TEXT NOT NULL,
 	reference_article INTEGER NOT NULL,
 	prix FLOAT NOT NULL,
-	stock INTEGER NOT NULL?
-	CONSTRAINT typ_art_pk PRIMARY KEY (reference_article)
+	stock INTEGER NOT NULL,
+	CONSTRAINT typ_art_pk PRIMARY KEY (nom_article)
 );
 
 CREATE TABLE Entrepot(
-	reference_article INTEGER,
+	reference_article INTEGER NOT NULL,
 	allee CHAR NOT NULL,
 	position INTEGER NOT NULL,
-	CONSTRAINT ent_pk PRIMARY KEY (reference_article)
+	CONSTRAINT ent_pk PRIMARY KEY (allee, position)
 );
 	
 	
