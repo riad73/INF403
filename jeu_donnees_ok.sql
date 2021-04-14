7a906955cef00b226c18aa493151cd81ca415cf6
Riad <58702474+riad73@users.noreply.github.com>
2021-04-14 20:56:39 +0200

    Update jeu_donnees_ok.sql

--- jeu_donnees_ok.sql
+++ jeu_donnees_ok.sql
@@ -2,7 +2,7 @@ INSERT INTO Clients VALUES (1, "Jason", "Kévin", "Rue des fleurs");
 INSERT INTO Clients VALUES (2, "Gorur", "Selim", "Rue des glandeurs");
 INSERT INTO Clients VALUES (3, "Guerbaa", "Rayan", "Rue du chomage");
 INSERT INTO Clients VALUES (4, "Bouissa", "Ilyass", "Rue du dormeur");
-INSERT INTO Clients VALUES (5, "Hadgverdi", "Nafiseh", "Rue de la gentilesse");
+INSERT INTO Clients VALUES (5, "Galva", "Jeremy", "Avenue de la foire");
 INSERT INTO Clients VALUES (6, "Fiori", "Victoria", "Rue du covid");
 
 
@@ -17,19 +17,21 @@ INSERT INTO Paniers VALUES (2,13);
 INSERT INTO Paniers VALUES (3,14);
 INSERT INTO Paniers VALUES (4,15);
 
-INSERT INTO Articles VALUES(12, 1, "NikeJordan", "A", 83);
-INSERT INTO Articles VALUES(13, 2, "Skateboard", "B", 10);
-INSERT INTO Articles VALUES(14, 3, "Lit", "A", 1);
-INSERT INTO Articles VALUES(15, 4, "Manteau", "G", 24);
-
-
 INSERT INTO Entrepot VALUES("A", 83, 12);
 INSERT INTO Entrepot VALUES("B", 10, 13);
 INSERT INTO Entrepot VALUES("A", 1, 14);
 INSERT INTO Entrepot VALUES("G", 24, 15);
-
+INSERT INTO Entrepot VALUES("G",1, 16);
 
 INSERT INTO TypeArticles VALUES("NikeJordan", 12, 199.99);
 INSERT INTO TypeArticles VALUES("Skateboard", 13, 19.83); 
 INSERT INTO TypeArticles VALUES("Lit", 14, 500);
 INSERT INTO TypeArticles VALUES("Manteau", 15, 50.2);
+INSERT INTO TypeArticles VALUES("Ordinateur", 16, 9.99);
+
+INSERT INTO Articles VALUES(12, "NikeJordan", "A", 83);
+INSERT INTO Articles VALUES(13, "Skateboard", "B", 10);
+INSERT INTO Articles VALUES(14, "Lit", "A", 1);
+INSERT INTO Articles VALUES(15, "Manteau", "G", 24);
+INSERT INTO Articles VALUES(16, "Ordinateur", "G", 1);
+
