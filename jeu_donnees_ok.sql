002bcd3d8408bf8f46517fa3c3bcf850db257103
Riad <58702474+riad73@users.noreply.github.com>
2021-04-14 16:40:07 +0200

    Create jeu_donnees_ok.sql

---
+++ jeu_donnees_ok.sql
@@ -0,0 +1,9 @@
+INSERT INTO Clients VALUES (1, "Jason", "Kévin", "Rue des fleurs");
+INSERT INTO Clients VALUES (2, "Gorur", "Selim", "Rue des glandeurs");
+INSERT INTO Clients VALUES (3, "Guerbaa", "Rayan", "Rue du chomage");
+INSERT INTO Clients VALUES (4, "Bouissa", "Ilyass", "Rue du dormeur");
+INSERT INTO Clients VALUES (5, "Hadgverdi", "Nafiseh", "5 ème étage");
+INSERT INTO Clients VALUES (6, "Fiori", "Victoria", "Rue du covid");
+
+
+INSERT INTO Commandes VALUES (1,1,"Rue des fleurs", '2021-04-14', "transit");
